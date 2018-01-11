eccb09aca5e3b375cb378e19a9ad57719b5c9228
Greg Myers <myersg86@gmail.com>
2018-01-11 04:39:32 -0800

    Update wp-phpcs.sh

--- wp-phpcs.sh
+++ wp-phpcs.sh
@@ -9,7 +9,8 @@ php phpcs -h
 curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
 chmod +x phpcbf.phar
 mv phpcbf.phar phpcbf
-php phpcbf -h
+curl -L http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -o php-cs-fixer
+sudo chmod a+x php-cs-fixer
 git clone -b master https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git ~/wpcs
 php phpcs --config-set installed_paths ~/wpcs
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -19,3 +20,6 @@ cd html
 ln -s ~/wp wp
 ln -s ~/phpcs phpcs
 ln -s ~/phpcbf phpcbf
+ln -s ~/php-cs-fixer php-cs-fixer
+php phpcs -h
+php phpcbf -h
